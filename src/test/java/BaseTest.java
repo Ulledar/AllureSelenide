e943117722f5d73d3abf2ec6fbc3fe3d933de0f9
Alex G <ulledar@ua.fm>
2021-03-15 17:24:00 +0200

    15/03 upd1

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,7 +7,7 @@ public class BaseTest {
 
     @BeforeEach
     public void start() {
-        Configuration.remote = "http://192.168.0.1:4444/wd/hub";
+        Configuration.remote = "http://192.168.0.121:4444/wd/hub";
         Configuration.driverManagerEnabled = false;
         //Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browser = "chrome";
